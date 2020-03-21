0a903a8cbda6fb763145b753f5f94d96dfff4e0c
Nikolas-crypto <nikolas.heinloth@student.uni-augsburg.de>
2020-03-21 12:24:29 +0100

    Funktionsstruktur

--- alpha-beta/lib/main.py
+++ alpha-beta/lib/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 #build the foundations and get familiar with programme
 # initialise board
@@ -7,20 +8,21 @@ def build_board():
     return board
 board=build_board()
 print(board)
-
+# flip the board
 def print_right_way (board):
     print(np.flip(board,0))
-
+# checks if colums are legal to be played
 def legal_check (board,selected_col):
     return board[5][selected_col] == 0
-
+# gives the row where piece lands
 def where_it_lands (board,selected_col):
     for r in range(6):
         if board [r][selected_col] == 0:
             return r
+# changes matrix entry
 def play (board, row, selected_col, piece):
     board[row][selected_col] = piece
-
+# deterines if there is a winner
 def so_won (board,piece):
     for c in range(4):
         for r in range(6):
@@ -40,7 +42,41 @@ def so_won (board,piece):
             if board[r][c] == piece and board[r - 1][c+1] == piece and board[r - 2][c+2] == piece and board[r - 3][
                 c+3] == piece:
                 return True
+# returns a value of an action
+def value_function (board, piece):
+    score=0
+    for r in range(6):
+        row_array=[int(i) for i in list(board[r,:])]
+        for c in range (4):
+            window= row_array[c:c+4]
+            if window.count(piece)==4:
+                score +=100
+            elif window.count(piece)==3 and window.count(0)== 1:
+                score += 30
+            elif window.count(piece)==2 and window.count(0)== 2:
+                score += 10
+    return score
 
+def valid_locations (board):
+    locations=[]
+    for selected_col in range(7):
+        if legal_check(board,selected_col):
+            locations.append(selected_col)
+    return locations
+
+def AI_move (board,piece):
+    locations=valid_locations(board)
+    best_score=0
+    col_move=random.choice(locations)
+    for selected_col in locations:
+        row= where_it_lands(board,selected_col)
+        copied_board=board.copy()
+        play(copied_board,row,selected_col,piece)
+        score= value_function(copied_board,piece)
+        if score> best_score:
+            best_score= score
+            col_move=selected_col
+    return col_move
 
 over=False
 turn=0
@@ -57,7 +93,7 @@ while not over:
                 over=True
 
     else:
-        selected_col = int(input("P2 choose (0-6):"))
+        selected_col = AI_move(board,2)
         if legal_check(board,selected_col):
             row= where_it_lands(board,selected_col)
             play(board,row,selected_col,2)
