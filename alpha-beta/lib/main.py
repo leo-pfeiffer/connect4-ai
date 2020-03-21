c6bcce5d028faabc2ce4dcd9a77c2310af4001bb
Nikolas-crypto <nikolas.heinloth@student.uni-augsburg.de>
2020-03-21 12:43:58 +0100

    Funktionsstruktur

--- alpha-beta/lib/main.py
+++ alpha-beta/lib/main.py
@@ -55,6 +55,16 @@ def value_function (board, piece):
                 score += 30
             elif window.count(piece)==2 and window.count(0)== 2:
                 score += 10
+    for c in range(7):
+        col_array=[int(i) for i in list(board[:,c])]
+        for r in range (3):
+            window= col_array[r:r+4]
+            if window.count(piece)==4:
+                score +=100
+            elif window.count(piece)==3 and window.count(0)== 1:
+                score += 30
+            elif window.count(piece)==2 and window.count(0)== 2:
+                score += 10
     return score
 
 def valid_locations (board):
