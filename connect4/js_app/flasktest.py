5594c17804689b64b3f82048267eb3aed742d76b
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-04-07 09:40:55 +0200

    GUI dev

--- connect4/js_app/flasktest.py
+++ connect4/js_app/flasktest.py
@@ -1,7 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from __future__ import print_function
+from flask import Flask, render_template, make_response
+from flask import redirect, request, jsonify, url_for
 
-@app.route('/')
-def hello_world():
-    # run code here and return selected move as dict
-    return {"row": 1, "col": 2}
+import io
+import os
+import uuid
+import numpy as np
+
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.debug = True
+app._static_folder = os.path.abspath("templates/static/")
+
+@app.route('/', methods=['GET'])
+def index():
+    title = 'Welcome to Teisendorf'
+    return render_template('connect_html.html',
+                           title=title)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
