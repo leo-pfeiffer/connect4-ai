2a7754b3d987efde255653cc3d7fe3682d563e39
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-04-19 16:47:22 +0200

    FINAL COMMIT

--- connect4/js_app/flasktest.py
+++ connect4/js_app/flasktest.py
@@ -13,12 +13,12 @@ app = Flask(__name__)
 app.secret_key = 's3cr3t'
 app.debug = True
 app._static_folder = os.path.abspath("templates/static/")
-webbrowser.open('http://0.0.0.0:5010/')
+webbrowser.open('http://0.0.0.0:5011/')
 
 @app.route('/', methods=['GET'])
 def index():
-    title = 'Welcome to Teisendorf'
-    return render_template('connect_html.html', title=title)
+    #title = 'Welcome to the Teisendorf-Challenge.\nCan you win against the AI?'
+    return render_template('connect_html.html')
 
 
 @app.route('/ai-action', methods=['GET', 'POST'])
@@ -84,4 +84,4 @@ def matrix_to_gameBoard(gameBoardMatrix):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5010)
+    app.run(host='0.0.0.0', port=5011)
